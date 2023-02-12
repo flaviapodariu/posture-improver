bb0336dbf54c4b5b7d2858219ef51c9a0817425b
Flav <79100216+flaviapodariu@users.noreply.github.com>
2023-02-12 14:15:19 +0200

    Delete app/build directory

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_licenta_postureimprover_PostureApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.licenta.postureimprover.PostureApplication"
-)
-public final class _com_licenta_postureimprover_PostureApplication {
-}
